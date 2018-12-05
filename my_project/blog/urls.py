ec700cdd80dca381e735d5e5ec63226d7b8140c4
chrisqiwei <christopherqiwei@gmail.com>
2018-12-05 18:29:53 +0800

    blog_demo
    p1-p4

---
+++ my_project/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from blog.views import home, posts, browser
+
+
+app_name = 'blog'
+urlpatterns = [
+    path('', home, name='home'),
+    path('/posts', posts, name='posts'),
+    path('/browser', browser, name='browser'),
+]
